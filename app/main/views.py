ab2244af9e946393d186683187abc0fe654c653c
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2018-02-16 11:37:35 -0500

    Module progress page

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,12 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for, jsonify
+from flask_login import current_user, login_required
 from ..models import EditableHTML
 
 from . import main
+from .. import db, csrf
 
+import logging
+import json
 
 @main.route('/')
 def index():
@@ -14,3 +18,22 @@ def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template('main/about.html',
                            editable_html_obj=editable_html_obj)
+
+@main.route('/modules')
+@login_required
+def modules():
+    return render_template('main/modules.html',
+                           modules=current_user.modules)
+
+@main.route('/modules-update', methods=['GET', 'POST'])
+@login_required
+@csrf.exempt
+def modules_update():
+    module_data = json.loads(request.form['data'])
+    new_module_string = ""
+    for i in range(len(current_user.modules)):
+        new_module_string += '1' if module_data[str(i+1)] else '0'
+    current_user.modules = new_module_string
+    db.session.commit()
+    flash('Your progress has been updated.', 'success')
+    return jsonify({'status': 200})
